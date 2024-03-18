9b42a3327c9b312ebd64588d4515facb84d71cfa
Viktoria Cherednichenko <superviktoria@ukr.net>
2024-03-18 16:54:56 +0200

    added lecture-20

--- exercises/lecture-20/main.js
+++ exercises/lecture-20/main.js
@@ -1,63 +1,65 @@
 // Створити об'єкт person, що має властивості name з типом рядка та age з типом цілого числа. Надати значення цим властивостям. 
 // Вивести значення властивостей об'єкту на консоль
 const person = {
-    name: "John",
-    age: 25     
+    name: "Viktoriya",
+    age: 35     
 };
 console.log("Name:", person.name);
 console.log("Age:", person.age);
-// Змінити тип властивості name з рядка на об'єкт, з ключами firstName та lastName. Надати значення цим властивостям. Вивести значення властивостей об'єкту на консоль.
-person.name = {
-    firstName: "John",
-    lastName: "Doe"
-};
 
-console.log("First Name:", person.name.firstName);
-console.log("Last Name:", person.name.lastName);
-// Створити метод об'єкту bio(), що виводить на консоль ім'я, прізвище та вік person.
-const person = {
+// Змінити тип властивості name з рядка на об'єкт, з ключами firstName та lastName. Надати значення цим властивостям. Вивести значення властивостей об'єкту на консоль.
+const person1 = {
     name: {
-        firstName: "John",
-        lastName: "Doe"
-    },
-    age: 25,
-    bio: function() {
-        console.log("First Name:", this.name.firstName);
-        console.log("Last Name:", this.name.lastName);
-        console.log("Age:", this.age);
+        firstName: "Viktoriya",
+        lastName: "Cherednichenko"
     }
 };
+console.log("First Name:", person1.name.firstName);
+console.log("Last Name:", person1.name.lastName);
 
-person.bio();
-// Створити метод об'єкту introduceSelf, що виводить на консоль Hi! I'm firstName.
-const person = {
+// Створити метод об'єкту bio(), що виводить на консоль ім'я, прізвище та вік person.
+const person2 = {
     name: {
-        firstName: "John",
-        lastName: "Doe"
+        firstName: "Viktoriya",
+        lastName: "Cherednichenko"
+    },
+    age: 35,
+    bio() {
+        return `First Name: ${this.name.firstName}\nLast Name: ${this.name.lastName}\nAge: ${this.age}`;
+    }
+
+};
+console.log(person2.bio());
+
+// Створити метод об'єкту introduceSelf, що виводить на консоль Hi! I'm firstName.
+const person3 = {
+    name: {
+        firstName: "Viktoriya",
+        lastName: "Cherednichenko"
     },
     age: 25,
-    introduceSelf: function() {
+    introduceSelf() {
         console.log("Hi! I'm " + this.name.firstName);
     }
 };
+person3.introduceSelf();
 
-person.introduceSelf();
 // Створити функцію-шаблон createPerson, що приймає аргумент name та повертає новий об'єкт з властивістю name та методом introduceSelf. Створити за допомогою createPerson 2 екземпляри об'єкта.
 
 function createPerson(name) {
     return {
         name: name,
-        introduceSelf: function() {
+        introduceSelf() {
             console.log("Hi! I'm " + this.name);
         }
     };
 }
 
-const person1 = createPerson("John");
-person1.introduceSelf();
+const person4 = new createPerson("Michael");
+person4.introduceSelf();
 
-const person2 = createPerson("Alice");
-person2.introduceSelf();
+const person5 = new createPerson("Kate");
+person5.introduceSelf();
 
 // Створити функцію-конструктор Person, що приймає аргумент name та повертає новий об'єкт з властивістю name та методом introduceSelf. Створити за допомогою Person 2 екземпляри об'єкта mary та tom.
 
@@ -75,11 +77,7 @@ const tom = new Person("Tom");
 tom.introduceSelf();
 
 // визначити, чи містить об'єкт mary властивість під назвою prop.
-if ('prop' in mary) {
-    console.log('Об\'єкт mary містить властивість prop.');
-} else {
-    console.log('Об\'єкт mary не містить властивості prop.');
-}
+console.log(Person.hasOwnProperty('prop'));
 // Брудний мартіні – ідеальний коктейль для любителів оливкового. Його можна приготувати на горілці чи джині за таким рецептом.
 //     ingredients:
 //         6 fluid ounces gin
@@ -105,21 +103,24 @@ if ('prop' in mary) {
 //         4 stuffed green olives
 
 const DirtyMartini = {
-    english_please: function() {
-        console.log("ingredients:");
-        console.log("6 fluid ounces gin");
-        console.log("1 dash dry vermouth (0.0351951ml)");
-        console.log("1 fluid ounce brine from olive jar");
-        console.log("4 stuffed green olives");
+    english_please() {
+        return `
+            ingredients:
+            6 fluid ounces gin
+            1 dash dry vermouth (0.0351951ml) 
+            1 fluid ounce brine from olive jar
+            4 stuffed green olives
+            `
     },
-    excuse_my_french: function() {
-        console.log("ingrédients:");
-        console.log("170.4786 ml de gin");
-        console.log("1 trait de vermouth sec (0.0351951ml)");
-        console.log("28.4131 ml de saumure du pot d'olive");
-        console.log("4 olives vertes farcies");
+    excuse_my_french() {
+        return `     
+            ingrédients:  
+            170.4786 ml de gin
+            1 trait de vermouth sec (0.0351951ml) 
+            28.4131 ml de saumure du pot d'olive
+            4 olives vertes farcies
+            `
     }
 };
-
-DirtyMartini.english_please();
-DirtyMartini.excuse_my_french();
+console.log(DirtyMartini.english_please());
+console.log(DirtyMartini.excuse_my_french());
