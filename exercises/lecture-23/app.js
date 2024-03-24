e362f594344847ce4c98d38b62efeb38ccbb217e
Viktoria Cherednichenko <superviktoria@ukr.net>
2024-03-25 01:17:19 +0200

    added lecture-23

--- exercises/lecture-23/app.js
+++ exercises/lecture-23/app.js
@@ -4,56 +4,54 @@ const alert = document.querySelector('.alert');
 // 1. Знайти на сторінці кнопку з класом btn-primary. Призначте знайденому елементу подію onclick. 
 // Написати функцію обробки події onclick, що додає CSS-клас alert-primary до елемента з id = alert та змінює значення 
 // властивості textContent цього елемента на "A simple primary alert—check it out!".
-{
+
 let btn = document.querySelector('.btn-primary');
-btn.addEventListener('onclick', function() {
-    let alertPrimery = document.getElementById('alert');
-    alertPrimery.classList.add('alert-primary');
-    alertPrimery.textContent = "A simple primary alert—check it out!";
+btn.addEventListener('onclick', () => {
+    
+    alert.classList.add('alert-primary');
+    alert.textContent = "A simple primary alert—check it out!";
 });
-}
+
+
 // 2. Знайти на сторінці кнопку з класом btn-secondary. Додати до кнопки прослуховувач події "click". 
 // Написати функцію обробки події click, що додає CSS-клас alert-primary до елемента з id = alert та змінює значення 
 // властивості textContent цього елемента на "A simple secondary alert—check it out!"
-{
-let btn = document.querySelector('.btn-secondary');
-btn.addEventListener('click', function() {
-    let alertPrimery = document.getElementById('alert');
-    alertPrimery.classList.add('alert-primary');
-    alertPrimery.textContent = "A simple secondary alert—check it out!";
+
+let btn1 = document.querySelector('.btn-secondary');
+btn1.addEventListener('click', () => {
+    alert.classList.add('alert-primary');
+    alert.textContent = "A simple secondary alert—check it out!";
 })
-}
+
 // 3. Знайти на сторінці кнопку з класом btn-success. Додати до кнопки прослуховувач події "mouseover". 
 // Написати функцію обробки події mouseover, що додає CSS-клас alert-success до елемента з id = alert та змінює 
 // значення властивості textContent цього елемента на "A simple success alert—check it out!"
 // Додати до кнопки прослуховувач події "mouseout". Написати функцію обробки події mouseout, що видаляє CSS-клас 
 // alert-success з елемента alert та змінює значення властивості textContent цього елемента на пустий рядок.
-{
-let btn = document.querySelector('.btn-success');
-let alertSuccess = document.getElementById('alert');
-btn.addEventListener('mouseover', function() {
-    alertSuccess.classList.add('alert-success');
-    alertSuccess.textContent = "A simple success alert—check it out!";   
+
+let btn2 = document.querySelector('.btn-success');
+btn2.addEventListener('mouseover', () => {
+    alert.classList.add('alert-success');
+    alert.textContent = "A simple success alert—check it out!";   
 })
-btn.addEventListener('mouseout', function() {
-    alertSuccess.classList.remove('alert-success');
-    alertSuccess.textContent = "";
+btn2.addEventListener('mouseout', () => {
+    alert.classList.remove('alert-success');
+    alert.textContent = "";
 });
-}
+
 // 4. Знайти на сторінці кнопку з класом btn-danger. Додати до кнопки прослуховувач події "focus". Написати функцію 
 // обробки події focus, що додає CSS-клас alert-danger до елемента з id = alert та змінює значення властивості 
 // textContent цього елемента на "A simple danger alert—check it out!" Додати до кнопки прослуховувач події "focusout". 
 // Написати функцію обробки події focusout, що видаляє CSS-клас alert-danger з елемента alert та змінює значення 
 //властивості textContent цього елемента на пустий рядок.
-let btn = document.querySelector('.btn-danger');
-let alertDanger = document.getElementById('alert');
-btn.addEventListener('focus', function() {
-    alertDanger.classList.add('alert-danger');
-    alertDanger.textContent = "A simple danger alert—check it out!";   
+let btn3 = document.querySelector('.btn-danger');
+btn3.addEventListener('focus', () => {
+    alert.classList.add('alert-danger');
+    alert.textContent = "A simple danger alert—check it out!";   
 })
-btn.addEventListener('focusout', function() {
-    alertDanger.classList.remove('alert-danger');
-    alertDanger.textContent = "";
+btn3.addEventListener('focusout', () => {
+    alert.classList.remove('alert-danger');
+    alert.textContent = "";
 });
 // 5. Знайти на сторінці кнопки з класом btn-dark та btn-light. Написати функцію toggleMode, що перемикає клас «dark-mode» 
 // елемента document.body за допомогою методу classList.toggle(). Одночасно при перемиканні класу dark-mode потрібно 
@@ -84,13 +82,13 @@ document.querySelector('.btn-light').addEventListener('click', toggleMode);
 // CSS-клас alert-info до елемента з id = alert та змінити значення властивості textContent цього елемента на 
 // "A simple info alert—check it out!";
 let btnInfo = document.querySelector('.btn-info');
-let alertEl = document.getElementById('alert');
+
 btnInfo.addEventListener('keypress', function(event) {
     if (event.key === "Enter") {
         event.preventDefault();
-        alertEl.classList.add('alert-info');
+        alert.classList.add('alert-info');
 
-        alertEl.textContent = "A simple info alert—check it out!";
+        alert.textContent = "A simple info alert—check it out!";
     }
 });
 // 7. Знайти на сторінці всі селектори .card. Використовуючи цмкл for, вивести на консоль вміст кожного елемента з класом 
@@ -113,7 +111,7 @@ let cards = document.querySelectorAll('.card');
 
 for (let i = 0; i < cards.length; i++) {
     let addToCartButton = cards[i].querySelector('.add-to-cart');
-    addToCartButton.addEventListener('click', function() {
+    addToCartButton.addEventListener('click', () => {
         let title = cards[i].querySelector('.card-title').textContent;
         console.log(title);
         });
