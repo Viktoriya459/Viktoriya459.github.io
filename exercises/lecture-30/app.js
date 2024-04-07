2a2fdf8145afc76040b26413174942a9a229672d
Viktoria Cherednichenko <superviktoria@ukr.net>
2024-04-07 23:14:18 +0300

    added lecture-30

--- exercises/lecture-30/app.js
+++ exercises/lecture-30/app.js
@@ -5,3 +5,51 @@ const template = (item) => `
 `;
 
 
+// 1. У файлі index.html є форма з 2-ма полями. Написати асинхронну функцію, що чекає 10 секунд, поки користувач заповнює 
+// поля форми. Після чого функція читає значення полів форми, зберігає їх у змінних firstName і lastName та перевіряє 
+// чи вони не пусті. Якщо обидва значення пусті, функція змінює контент h1 на I'm miss You. Якщо хоча б одне зі значень 
+// заповнене, функція змінює контент h1 на Hello firstName lastName!
+function waitForUserInput() {
+    setTimeout(function() {
+        const firstNameInput = document.querySelector('input[name="firstName"]');
+        const lastNameInput = document.querySelector('input[name="lastName"]');
+
+        const firstName = firstNameInput.value.trim();
+        const lastName = lastNameInput.value.trim();
+
+        if (firstName === '' && lastName === '') {
+            document.getElementById('waiting').textContent = "I'm miss You";
+        } else {
+            document.getElementById('waiting').textContent = `Hello ${firstName} ${lastName}!`;
+        }
+    }, 10000);
+}
+
+waitForUserInput();
+
+
+// 2. Створити новий об'єкт xhr, як екземпляр XMLHttpRequest. Надіслати запит на веб-сервер https://jsonplaceholder.typicode.
+// com/posts, використовуючи методи open() і send().
+
+let xhr = new XMLHttpRequest();
+xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
+xhr.send();
+// Використовуючи подію xhr.onload, напишіть функцію зворотного виклику, що отримує результат запиту з сервера, за 
+// допомогою JSON.parse перетворює результат на об'єкт, будує за допомогою шаблона template стрічку публікацій та 
+// поміщає отриманий результат всередину елемента з id="demo".
+
+xhr.onload = function() {
+  const response = JSON.parse(xhr.responseText);
+  let postsHTML = '';
+  response.forEach(post => {
+    postsHTML += `<div class="post">
+                    <h2>${post.title}</h2>
+                    <p>${post.body}</p>
+                  </div>`;
+  });
+  document.getElementById('demo').innerHTML = postsHTML;
+};
+
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+xhr.send();
+    
