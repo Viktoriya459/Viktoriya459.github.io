b5753a9c28fc41b453061572763213f98d0f8aa9
Viktoria Cherednichenko <superviktoria@ukr.net>
2024-04-08 14:44:37 +0300

    added lecture-30

--- exercises/lecture-30/app.js
+++ exercises/lecture-30/app.js
@@ -1,10 +1,4 @@
 
-const template = (item) => `
-<h3>${item.title}</h3>
-<div>${item.body}</div>
-`;
-
-
 // 1. У файлі index.html є форма з 2-ма полями. Написати асинхронну функцію, що чекає 10 секунд, поки користувач заповнює 
 // поля форми. Після чого функція читає значення полів форми, зберігає їх у змінних firstName і lastName та перевіряє 
 // чи вони не пусті. Якщо обидва значення пусті, функція змінює контент h1 на I'm miss You. Якщо хоча б одне зі значень 
@@ -38,14 +32,15 @@ xhr.send();
 // допомогою JSON.parse перетворює результат на об'єкт, будує за допомогою шаблона template стрічку публікацій та 
 // поміщає отриманий результат всередину елемента з id="demo".
 
+const template = (item) => `
+<h3>${item.title}</h3>
+<div>${item.body}</div>
+`;
 xhr.onload = function() {
   const response = JSON.parse(xhr.responseText);
   let postsHTML = '';
   response.forEach(post => {
-    postsHTML += `<div class="post">
-                    <h2>${post.title}</h2>
-                    <p>${post.body}</p>
-                  </div>`;
+    postsHTML += template(post);
   });
   document.getElementById('demo').innerHTML = postsHTML;
 };
