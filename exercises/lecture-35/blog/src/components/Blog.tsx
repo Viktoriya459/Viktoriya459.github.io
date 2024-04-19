981cd3f8d778276aa6c708e81a233ff597131711
Viktoria Cherednichenko <superviktoria@ukr.net>
2024-04-19 16:45:47 +0300

    added lecture-35

---
+++ exercises/lecture-35/blog/src/components/Blog.tsx
@@ -0,0 +1,42 @@
+// src/components/Blog.tsx
+
+
+export default function Blog({ post }) {
+    return (
+        <article className='post'>
+            <div className='cover-container'>
+              <img src={post.cover} alt={post.title} />
+            </div>
+            <div className='post-footer'>
+              <h3>{post.title} {post.id}</h3>
+              <p>{post.content}</p>
+            </div>
+        </article>
+    );
+}
+
+
+
+// class Blog extends React.Component {
+
+//     constructor(props:any) {
+//         super(props);
+//         this.state = { post: props.post };
+//     }
+
+//     render() {
+//        return(
+//         <article className='post'>
+//             <div className='cover-container'>
+//               <img src={this.props.post.cover} alt={this.props.post.title} />
+//             </div>
+//             <div className='post-footer'>
+//               <h3>{this.props.post.title} {this.props.post.id}</h3>
+//               <p>{this.props.post.content}</p>
+//             </div>
+//         </article>
+//         );
+//     }
+// }
+// export default Blog;
+
