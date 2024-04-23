5242e0122874c4bc8e69bcf4351c35b7b6fd9ff8
Viktoria Cherednichenko <superviktoria@ukr.net>
2024-04-23 23:13:54 +0300

    Added lecture-36

--- exercises/lecture-36/blog/src/components/Post.jsx
+++ exercises/lecture-36/blog/src/components/Post.jsx
@@ -1,5 +1,15 @@
 import {useState, useEffect} from "react"
+// Використовуючи хук useState, виконати ініціалізацію значень стану та метода зміни стану post, setPost, 
+// передавши в useState порожній масив.
 
+// Напишіть функцію fetchData(), що отримує дані з 'https://my-json-server.typicode.com/<ваш власний акаунт 
+// на github>/db/posts/1', використовуючи метод fetch. Зробіть функцію асинхронною.
+
+// Використовуючи хук useEffect, викличте функцію fetchData(), передавши її як перший аргумент хука useEffect. 
+// Передайте порожній масив як другий аргумент хука useEffect.
+
+// Напишіть функцію likeThis(), що змінює стан змінної likes, збільшуючи її на 1. Використовуючи хук useEffect, 
+// напишіть обробку події натискання на кнопку з ідентифікатором like, що викликає функцію likeThis.
 function Post() {
     const [post, setPost] = useState({});
     const [likes, setLikes] = useState(0);
@@ -8,7 +18,6 @@ function Post() {
         async function fetchData() {
             const response = await fetch('https://my-json-server.typicode.com/Viktoriya459/db/posts/1');
             const postData = await response.json();
-            console.log(postData);
             setPost(postData);
         }
         fetchData();
---
+++ exercises/lecture-37/blog/src/components/Post.jsx
@@ -0,0 +1,53 @@
+import {useState, useEffect, useContext, createContext} from "react"
+// Потрібно створити контекст PostContext Потрібно створити компонент Layout 
+// Потрібно створити компонент Header Потрібно створити компонент PostTitle Потрібно створити компонент PostMain
+const PostContext = createContext({});
+
+const Layout = ({ children }) => {
+    return <div className="layout">{children}</div>;
+  };
+  
+  const Header = () => {
+    const post = useContext(PostContext);
+    return <header>{post.cover}</header>;
+  };
+  
+  const PostTitle = () => {
+    const post = useContext(PostContext);
+    return <h1>{post.title}</h1>;
+  };
+  
+  const PostMain = () => {
+    const post = useContext(PostContext);
+    return <main>{post.content}</main>;
+  };
+  
+  const Post = () => {
+    const [post, setPost] = useState({});
+  
+    useEffect(() => {
+      async function fetchData() {
+        const response = await fetch('https://my-json-server.typicode.com/Viktoriya459/db/posts/1');
+        const post= await response.json();
+        setPost(post);
+      }
+      fetchData();
+    }, []);
+  
+    return (
+      <PostContext.Provider value={post}>
+        <Layout>
+          <Header />
+          <PostTitle />
+          <PostMain />
+        </Layout>
+      </PostContext.Provider>
+    );
+  };
+  
+  export default Post;
+
+
+
+
+
