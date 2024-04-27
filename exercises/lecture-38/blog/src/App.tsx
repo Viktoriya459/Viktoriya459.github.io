da2e10dd306faeea91e8da36667c1acb571730aa
Viktoria Cherednichenko <superviktoria@ukr.net>
2024-04-27 10:47:09 +0300

    Added lecture-38

--- exercises/lecture-38/blog/src/App.tsx
+++ exercises/lecture-38/blog/src/App.tsx
@@ -1,15 +1,17 @@
 // src/App.tsx
+import './App.css'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Posts from './components/Posts';
+import Blog from './pages/blog';
 
 function App() {
 
   return (
     <>
 
-      <Posts />
+      <Blog />
     </>
   )
 }
